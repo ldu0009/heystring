9d3d1367df6c2b436a14394ca2f3ad2f022661df
woky <ldu0009@gmail.com>
2024-10-23 21:04:37 +0900

    1차 main page

--- src/app/components/pages/main/main.component.ts
+++ src/app/components/pages/main/main.component.ts
@@ -1,18 +1,19 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
-
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, type OnInit } from "@angular/core";
+import { YouTubePlayerModule } from "@angular/youtube-player";
+import * as pageData from "../../../../assets/data/main.json";
 @Component({
-  selector: 'app-main',
+  selector: "app-main",
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.scss',
+  imports: [CommonModule, YouTubePlayerModule],
+  templateUrl: "./main.component.html",
+  styleUrl: "./main.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent implements OnInit {
+  public readonly pageData = pageData;
 
-  ngOnInit(): void { }
-
+  ngOnInit(): void {
+    console.log(this.pageData);
+  }
 }
